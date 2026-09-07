e63258611769ecf07bfd7db608e4de45808829c4
Ashwani Pundir <ashwanip44@gmail.com>
2026-09-07 16:43:01 +0530

    Test1

--- src/main/java/com/example/demo/controller/RestControllerClass.java
+++ src/main/java/com/example/demo/controller/RestControllerClass.java
@@ -1,17 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/welcome")
 public record RestControllerClass() {
 
-    @GetMapping
+    @GetMapping("/api/welcome")
     public ResponseEntity<String> welcome() {
         return ResponseEntity.ok("Welcome to the Spring Boot REST API!");
+    }
+
+    @GetMapping("/api/welcome/{name}")
+    public ResponseEntity<String> welcomeWithName(String name) {
+        return ResponseEntity.ok("Welcome to the Spring Boot REST API, " + name + "!");
     }
 }
